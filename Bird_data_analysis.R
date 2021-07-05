7815964c3462f9796898b932ead32299a656a0b7
PreciousPossum <lina.w@posteo.de>
2021-07-05 12:34:06 +0200

    script fitted to new dataset

--- Bird_data_analysis.R
+++ Bird_data_analysis.R
@@ -15,16 +15,16 @@ dat <- read.csv("data/birds_dataset.csv", sep = ";", header=TRUE, dec = ".")
 dat$category <- as.factor(dat$category)
 
 ## adding richness and abundances
-dat$rich<-specnumber(dat[,5:32]) #species richness
-dat$abund<-rowSums(dat[,5:32]) #abundances
-dat$rarerich<-rarefy(dat[,5:32],min(dat$abund)) #rarefied richness based on the subsample with the lowest number of individuals
+dat$rich<-specnumber(dat[,4:31]) #species richness
+dat$abund<-rowSums(dat[,4:31]) #abundances
+dat$rarerich<-rarefy(dat[,4:31],min(dat$abund)) #rarefied richness based on the subsample with the lowest number of individuals
 
 ## looking at the data
 boxplot(rich~category,data=dat)
 boxplot(log(abund+1)~category,data=dat)
 boxplot(rarerich~category,data=dat)
 
-hist(rowSums(bird),
+hist(rowSums(dat[,4:31]),
      col = "grey", # colors of bins
      main = "Bird Community Sampling", # plot title
      xlab = "Number of individuals") # x-axis title
@@ -32,8 +32,8 @@ hist(rowSums(bird),
 #### SPECIES ACCUMULATION CURVE ####
 # how good was sampling effort in the ecosystem types to assess species richness? 
 
-SAC_park <- specaccum(subset(bird, env$category == "park"))
-SAC_fore <- specaccum(subset(bird, env$category == "forest"))
+SAC_park <- specaccum(subset(dat[,4:31], dat$category == "park"))
+SAC_fore <- specaccum(subset(dat[,4:31], dat$category == "forest"))
 
 plot(SAC_park, xlab = "plots", ylab = "species richness", main="species accumulation curve")
 plot(SAC_fore, xlab = "plots", ylab = "species richness", main="species accumulation curve", col="green", add = T)
@@ -48,11 +48,11 @@ abline(0, 1) # 1:1 line
 
 #### ORDINATION WITH NMDS ####
 # to check for differences in species composition
-nmd1<-metaMDS(dat[,5:28],distance="horn",k=2) #NMDS analysis based on Morisita-Horn-Index as a dissimilarity measure
+nmd1 <- metaMDS(dat[,4:31],distance="horn",k=2) #NMDS analysis based on Morisita-Horn-Index as a dissimilarity measure
 plot(nmd1,display="species",type="t") #plot results
 points(nmd1,pch=c(16,17)[as.numeric(as.factor(dat$category))],cex=1.4) #add sampling points
 legend("topright",pch=c(16,17),c("Forest","Park"), cex = 0.7) #add legend
-ef<-envfit(nmd1,dat[,33:43]) #check for correlation of dissimilarity gradients with environmental variables
+ef <- envfit(nmd1,dat[,32:44]) #check for correlation of dissimilarity gradients with environmental variables
 ef #results
 plot(ef,p.max=0.05) #add significant environmental variables to the NMDS plot
 
